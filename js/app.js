b4a06d216a1e41c95f2373c353fb5e8d2437aebc
Benjamin White <benjamin.e.white1@gmail.com>
2014-01-22 00:45:02 -0500

    refined demo page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,79 @@
-function playHadouken(){hadoukenSound=!hadoukenSound,hadoukenSound&&($("#hadouken-sound")[0].load(),$("#hadouken-sound")[0].play())}function playGame(){$(".ryu").mouseenter(function(){$(".ryu-action").hide(),$(".ryu-ready").show()}).mousedown(function(){playHadouken(),$(".ryu-action").hide(),$(".ryu-throwing").show(),$(".hadouken").finish().show().animate({"margin-left":"1000px"},1e3,function(){$(this).stop(),$(this).hide(),$(this).css("margin-left",420)})}).mouseup(function(){$(".ryu-action").hide(),$(".ryu-ready").show()}),$(document).keydown(function(a){88==a.keyCode&&($(".ryu-action").hide(),$(".ryu-cool").show())}).keyup(function(a){88==a.keyCode&&($(".ryu-cool").hide(),$(".ryu-ready").show())})}function doIntro(){$("#theme-song")[0].play(),$(".sf-logo").fadeIn(3500,function(){$(this).fadeOut(1e3,function(){$(".brought-by").fadeIn(1500,function(){$(this).fadeOut(1e3,function(){$(".jquery-logo").fadeIn(1500,function(){$(this).fadeOut(1500,function(){$(".how-to").fadeIn(1e3,function(){$(this).delay(5e3).fadeOut(500)})})})})})})})}var hadoukenSound=!1;$(document).ready(function(){doIntro(),playGame()});
+$(document).ready(function() {
+	doIntro();
+	playGame();
+});
+
+var hadoukenSound = false;
+function playHadouken () {
+  hadoukenSound = !hadoukenSound;
+  if (hadoukenSound) {
+    $('#hadouken-sound')[0].load()
+    $('#hadouken-sound')[0].play();
+  }
+}
+
+var coolSound = false;
+function playCool () {
+  coolSound = !coolSound;
+  if (coolSound) {
+    // $('#cool')[0].load()
+    $('#cool')[0].play();
+  }
+}
+
+
+function playGame() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-action').hide();
+    $('.ryu-ready').show();
+  })
+  .mousedown(function() {
+    playHadouken();
+    $('.ryu-action').hide();  
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+      .animate(
+        {'margin-left': '800px'},
+        800,
+        function() {
+          $(this).stop();
+          $(this).hide();
+          $(this).css('margin-left', 420);
+        }
+      );
+  })
+  .mouseup(function() {
+    $('.ryu-action').hide();
+    $('.ryu-ready').show();
+  });
+
+  $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      playCool();
+      $('.ryu-action').hide();
+      $('.ryu-cool').show();
+    }   
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-ready').show();
+    }
+  });
+}
+
+function doIntro() {
+  $('#theme-song')[0].play();
+  $('.sf-logo').fadeIn(3500, function() {
+    $(this).fadeOut(1000, function() {
+      $('.brought-by').fadeIn(1500, function() {
+        $(this).fadeOut(1000, function() {
+          $('.jquery-logo').fadeIn(1500, function() {
+            $(this).fadeOut(1500, function() {
+              $('.how-to').fadeIn(1000);
+            });
+          })
+        })
+      })
+    })
+  })
+}
